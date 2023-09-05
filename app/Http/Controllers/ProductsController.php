3f016c52344b4772fdbe5fa5fc569b598ac1bfe2
Zann181 <sazambranoc@unal.edu.co>
2023-09-04 20:58:40 -0500

    Primera Parte

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+            $this->validate($request, [
+                'name' => 'required|string',
+                'category_id' => 'required|integer',
+            ]);
+        
+            $product = new Product();
+            $product->name = $request->name;
+            $product->category_id = $request->category_id;
+            $product->save();
+        
+            return redirect()->route('products.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
